a48597201ccf6cc89379750366fe11e455f22a80
jschuler93 <jhs5446@rit.edu>
2015-11-12 21:53:43 -0500

    Update CreateDDLSqlServer.java

--- src/applicationdev/CreateDDLSqlServer.java
+++ src/applicationdev/CreateDDLSqlServer.java
@@ -1,4 +1,7 @@
 
+import javax.swing.JOptionPane;
+
+
 public class CreateDDLSqlServer extends EdgeConvertCreateDDL {
 
     protected String databaseName;                                            //the name of databases
@@ -33,4 +36,45 @@ public class CreateDDLSqlServer extends EdgeConvertCreateDDL {
     public void createDDL() {
 
     }
+    
+    /**
+     * converts a String boolean used in SqlServer to an int SqlServer uses '1' and '0'
+     * for boolean types
+     *
+     * @return int
+     */
+    protected int convertStrBooleanToInt(String input) {
+        if (input.equals("true")) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }//end convertStrBooleanToInt
+   
+   /**
+   * generates the database name by prompting user for the name
+   * @return this.databaseName String 
+   */
+   public String generateDatabaseName() { 
+      String dbNameDefault = "SqlServerDB"; //default for this.databaseName
+	  
+      do { //asks for the database name
+         this.databaseName = (String)JOptionPane.showInputDialog(
+                       null,
+                       "Enter the database name:",
+                       "Database Name",
+                       JOptionPane.PLAIN_MESSAGE,
+                       null,
+                       null,
+                       dbNameDefault);
+         if (this.databaseName == null) {
+            EdgeConvertGUI.setReadSuccess(false);
+            return "";
+         }
+         if (this.databaseName.equals("")) {
+            JOptionPane.showMessageDialog(null, "You must select a name for your database.");
+         }
+      } while (this.databaseName.equals(""));
+      return this.databaseName;
+   }//end generateDatabaseName
 }
